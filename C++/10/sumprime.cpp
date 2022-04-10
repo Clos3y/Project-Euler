de577a17bee0f9f0d97f106d4c0eeec81e2065e5
Sam Close <sam.w.close@gmail.com>
2022-04-10 22:12:44 +0100

    10

---
+++ C++/10/sumprime.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+bool is_prime( int n ){
+
+	if ( n % 2 == 0 ) { 
+		//even
+		return 0;
+
+	} else {
+
+		int root_n = lrint(sqrt(n));
+
+		for ( int i = 3, inc = 2; i <= root_n; i += inc){
+
+			if (n % i == 0) {
+
+			return 0;
+
+			};
+
+		}; //prime
+	};
+
+	return 1;
+};
+
+long int sum_lim( int n ) {
+
+	int p = 3;		
+
+	long int sum = 2;
+
+	while (p < n) {
+
+		if (is_prime(p)) {
+		
+			sum += p;
+	
+		};	
+
+		p += 2;
+	};
+	
+	return sum;
+};
+
+int main() {
+
+cout << sum_lim(2000000);
+cout << "\n";
+
+return 0;
+
+};
