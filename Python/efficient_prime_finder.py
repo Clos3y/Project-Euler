99b77939aff176f5e22dcd87aac400ad24aace0f
Sam <39468367+Clos3y@users.noreply.github.com>
2023-03-29 10:29:25 +0100

    Update efficient_prime_finder.py

--- Python/efficient_prime_finder.py
+++ Python/efficient_prime_finder.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 from math import isqrt
 
-@functools.lru_cache
+@lru_cache
 def is_prime(n: int) -> bool:
   for i in range(2,isqrt(n)):
     if is_prime(i) and n % i == 0:
