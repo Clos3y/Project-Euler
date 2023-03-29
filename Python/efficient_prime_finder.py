41a4ff51169e801f8060d3676dc3941ca21ff4f4
clos3y <sam.w.close@gmail.com>
2023-03-29 21:26:16 +0100

    Fixed prime finder

--- Python/efficient_prime_finder.py
+++ Python/efficient_prime_finder.py
@@ -3,7 +3,12 @@ from math import isqrt
 
 @lru_cache
 def is_prime(n: int) -> bool:
-  for i in range(2,isqrt(n)):
-    if is_prime(i) and n % i == 0:
-      return False
-  return True
+    if n == 1:
+        return False
+    elif n == 2:
+        return True
+    else:
+        for i in range(2,isqrt(n)+1):
+            if is_prime(i) and n % i == 0:
+                return False
+        return True
